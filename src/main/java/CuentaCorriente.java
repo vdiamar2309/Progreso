283168d016ca73fc388e4e22be2689fbf1fa5dba
Victor <vdiamar2309@g.educaand.es>
2025-11-20 14:13:41 +0100

    modified:   src/main/java/CuentaCorriente.java 	modified:   src/main/java/Principal.java

--- src/main/java/CuentaCorriente.java
+++ src/main/java/CuentaCorriente.java
@@ -1,10 +1,11 @@
-public class CuentaCorriente {
+public class   CuentaCorriente {
     private double saldo;
     private int contadorReintegros,numIngreso;
-    private double dineroFisico;
+    public double dineroFisico;
     // Constructor
     public CuentaCorriente(double saldoInicial){
         saldo=saldoInicial;
+        dineroFisico=0;
     }
 
     private void setSaldo(double saldo) throws Miexception {
@@ -21,7 +22,23 @@ public class CuentaCorriente {
         if (cantidad>saldo){
             throw new Miexception("No puedes retirar una cantidad mayor a la que tienes en tu cuenta");
         }
+        saldo=dineroFisico;
         saldo-=cantidad;
         contadorReintegros++;
     }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void ingresar(double cantidad) throws Miexception{
+        if (cantidad<0){
+            throw new Miexception("No puedes ingresar una cantidad menor de 0");
+        }
+        if (cantidad>dineroFisico){
+            throw new Miexception("No puedes ingresar una cantidad mayor a la que tienes");
+        }
+        numIngreso++;
+    }
+
 }
--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,17 +1,19 @@
-import java.util.Scanner;
 import MiEntradaSalida.MiEntradaSalida;
+
+import java.util.Scanner;
+
 public class Principal {
 
     public static String ayuda = "¿Que acción quiere realizar? (Puede retirar, ingresar o ver tu saldo o salir)";
-    public static String retiro= "¿Cuanto desea retirar?";
-    public static String hecho= "La acción ha sido realizada ¿quiere hacer alguna más?";
+    public static String retiro = "¿Cuanto desea retirar?";
+    public static String hecho = "La acción ha sido realizada ¿quiere hacer alguna más?";
 
-   public static void main() {
+    static void main() {
         CuentaCorriente c = new CuentaCorriente(6);
         Scanner scanner = new Scanner(System.in);
         boolean iniciar = true;
-       System.out.println("Bienvenido al Banco");
-       System.out.println("Escribe man para abrir la ayuda");
+        System.out.println("Bienvenido al Banco");
+        System.out.println("Escribe man para abrir la ayuda");
         while (iniciar) {
             System.out.print("> ");
             String accion = scanner.nextLine();
@@ -20,7 +22,7 @@ public class Principal {
                     System.out.println(ayuda);
                     break;
                 case "retirar":
-                    int banco= MiEntradaSalida.solicitarEntero(retiro);
+                    int banco = MiEntradaSalida.solicitarEntero(retiro);
                     double saldo = banco;
                     try {
                         c.retirar(saldo);
@@ -30,6 +32,8 @@ public class Principal {
                     }
 
                     break;
+                case "ingresar":
+                    int
             }
         }
     }
