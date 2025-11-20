1b20cad8d9d757ffe8a34b97f50a408a62cf78d9
Victor <vdiamar2309@g.educaand.es>
2025-11-20 12:06:22 +0100

    modified:   src/main/java/Principal.java

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Principal {
 
     public static String ayuda = "¿Que acción quiere realizar? (Puede retirar, ingresar o ver tu saldo o salir)";
+    public static String retiro= "¿Cuanto desea retirar?";
+    public static String hecho= "La acción ha sido realizada ¿quiere hacer alguna más?";
 
-    static void main() {
+   public static void main() {
         CuentaCorriente c = new CuentaCorriente(6.7);
         Scanner scanner = new Scanner(System.in);
         boolean iniciar = true;
@@ -12,11 +14,14 @@ public class Principal {
         while (iniciar) {
             System.out.print("> ");
             String accion = scanner.nextLine();
+            switch (accion) {
+                case "Retirar":
+                    System.out.println(retiro);
 
-
+            }
         }
+
+
     }
 
-
-}
 }
